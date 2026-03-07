6fc5b8e48541825a1f704274fe31a03a79cbb7c3
jiwoo <jwo2955@gmail.com>
2026-03-07 16:35:20 +0900

    fix: OAuth 로그인 권한 조회 오류 수정

--- src/main/java/com/january/guestbook/security/service/MemberOAuth2UserService.java
+++ src/main/java/com/january/guestbook/security/service/MemberOAuth2UserService.java
@@ -2,6 +2,7 @@ package com.january.guestbook.security.service;
 
 import com.january.guestbook.domain.Member;
 import com.january.guestbook.entity.MemberRole;
+import com.january.guestbook.mapper.MemberAuthMapper;
 import com.january.guestbook.mapper.MemberMapper;
 import com.january.guestbook.security.dto.AuthMemberDTO;
 import lombok.RequiredArgsConstructor;
@@ -22,24 +23,25 @@ import java.util.stream.Collectors;
 public class MemberOAuth2UserService extends DefaultOAuth2UserService {
 
     private final MemberMapper memberMapper;
+    private final MemberAuthMapper memberAuthMapper;
     private final PasswordEncoder passwordEncoder;
 
     @Override
     public OAuth2User loadUser(OAuth2UserRequest request) throws OAuth2AuthenticationException {
 
         log.info("--------------------------------------------");
-        log.info("userRequest: " + request);
+        log.info("userRequest: {}", request);
 
         String clientName = request.getClientRegistration().getClientName();
 
-        log.info("clientName: " + clientName);
+        log.info("clientName: {}", clientName);
         log.info(request.getAdditionalParameters());
 
         OAuth2User user = super.loadUser(request);
 
         log.info("==========================");
         user.getAttributes().forEach((k, v) -> {
-            log.info(k + ": " + v); // 구글에 프로젝트를 등록하면서 지정한 'API 범위' 항목과 application-oauth.yml 설정 파일과 관련됨.
+            log.info("{}: {}", k, v); // 구글에 프로젝트를 등록하면서 지정한 'API 범위' 항목과 application-oauth.yml 설정 파일과 관련됨.
         });
 
         String email = null;
@@ -48,7 +50,7 @@ public class MemberOAuth2UserService extends DefaultOAuth2UserService {
             email = user.getAttribute("email");
         }
 
-        log.info("email: " + email);
+        log.info("email: {}", email);
 
         Member member = saveSocialMember(email);
 
@@ -70,7 +72,7 @@ public class MemberOAuth2UserService extends DefaultOAuth2UserService {
     private Member saveSocialMember(String email) {
 
         // 기존에 동일한 이메일로 가입한 회원이 있는 경우에는 그대로 조회만
-        Member member = memberMapper.getMemberByEmail(email);
+        Member member = memberAuthMapper.getAuthMemberByEmail(email);
 
         if (member != null) {
             return member;
